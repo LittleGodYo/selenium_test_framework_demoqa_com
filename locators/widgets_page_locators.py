d1c88f93124f9d7c7be4a2cd2dfc102fdcf0bc80
111 <mrlittlegod@gmail.com>
2022-06-29 00:06:32 +0300

    created: widgets_page.py, widgets_page_locators.py, widgets_test.py
    added test:
    -test_accordian

---
+++ locators/widgets_page_locators.py
@@ -0,0 +1,7 @@
+class AccordianPageLocators:
+    SECTION_FIRST = "div[id='section1Heading']"
+    SECTION_CONTENT_FIRST = "div[id='section1Content'] p"
+    SECTION_SECOND = "div[id='section2Heading']"
+    SECTION_CONTENT_SECOND = "div[id='section2Content'] p"
+    SECTION_THIRD = "div[id='section3Heading']"
+    SECTION_CONTENT_THIRD = "div[id='section3Content'] p"
